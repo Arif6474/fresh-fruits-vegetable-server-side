c61a9ea3721d8bbe987b4e9ecda5ff103da9910b
Arif6474 <arifulislam64743@gmail.com>
2022-05-07 03:11:05 +0600

    added fruit details

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 
@@ -29,7 +29,14 @@ async function run() {
       const fruits = await cursor.toArray();
       res.send(fruits);
       })
-      //get 
+      //get fruits details
+    app.get('/fruit/:id' , async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}
+      const fruit= await fruitsCollection.findOne(query);
+      res.send(fruit);
+
+    })   
    
      
      
